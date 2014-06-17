ffafc336a7a9e2d5afe890412edc324d2e9b4192
boozz <shapovdu@yandex.ru>
2014-06-17 22:03:40 +0300

    Zend App Test

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -19,4 +19,8 @@ class Album
         $this->artist = (isset($data['artist'])) ? $data['artist'] : null;
         $this->title  = (isset($data['title'])) ? $data['title'] : null;
     }
+
+    public function  getArrayCopy(){
+        return get_object_vars($this);
+    }
 }
